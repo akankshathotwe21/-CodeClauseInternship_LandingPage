dba398b17c8c0523ea3ad110c7656d7ce1d70959
akankshathotwe21 <141600760+akankshathotwe21@users.noreply.github.com>
2023-08-24 08:35:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const bookButton = document.getElementById("bookButton");
+    bookButton.addEventListener("click", function () {
+        alert("Thank you for choosing HolliTravel! We can't wait to help you plan your dream trip.");
+    });
+});
